e8ca0d781729eb869b52f710e16a32a4958de171
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:19:20 -0300

    1010 Resolvido
    Solução problema 1010

---
+++ Csharp_accepted_solutions/Bee1010.cs
@@ -0,0 +1,15 @@
+﻿int codigoPeca1,codigoPeca2, numeroPecas1, numeroPecas2;
+double valorPeca1, valorPeca2, valorTotal;
+
+string [] valores = Console.ReadLine().Split(' ');
+codigoPeca1 = int.Parse(valores[0]);
+numeroPecas1 = int.Parse(valores[1]);
+valorPeca1 = double.Parse(valores[2]);
+
+valores = Console.ReadLine().Split(' ');
+codigoPeca2 = int.Parse(valores[0]);
+numeroPecas2 = int.Parse(valores[1]);
+valorPeca2 = double.Parse(valores[2]);
+
+valorTotal = (numeroPecas1 * valorPeca1)+(numeroPecas2 * valorPeca2);
+Console.WriteLine($"VALOR A PAGAR: R$ {Convert.ToDecimal(valorTotal).ToString("0.00")}");
