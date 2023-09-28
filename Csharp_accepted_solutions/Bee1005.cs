3fe0c7f0ceff6257e49741afa1f21b1ea7bf8e61
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:16:50 -0300

    1005 Resolvido
    Solução problema 1005

---
+++ Csharp_accepted_solutions/Bee1005.cs
@@ -0,0 +1,10 @@
+﻿double a,b,media,peso1,peso2;
+peso1 = 3.5;
+peso2 = 7.5;
+
+a = double.Parse(Console.ReadLine()) * peso1;
+b = double.Parse(Console.ReadLine()) * peso2;
+
+media = (a + b) / (peso1+peso2);
+
+Console.WriteLine($"MEDIA = {Convert.ToDecimal(media).ToString("0.00000")}");
