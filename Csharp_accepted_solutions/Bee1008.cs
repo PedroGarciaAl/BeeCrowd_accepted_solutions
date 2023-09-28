ab1a6b8521a5836bcd742bacbd4523a1dd796971
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:18:17 -0300

    1008 Resolvido
    Solução problema 1008

---
+++ Csharp_accepted_solutions/Bee1008.cs
@@ -0,0 +1,12 @@
+﻿int numeroFuncionario, horasTrabalhadas;
+double valorPorHora, salarioFuncionario;
+
+numeroFuncionario = Convert.ToInt32(Console.ReadLine());
+horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+valorPorHora = double.Parse(Console.ReadLine());
+
+salarioFuncionario = horasTrabalhadas * valorPorHora;
+
+
+Console.WriteLine($"NUMBER = {numeroFuncionario}");
+Console.WriteLine($"SALARY = U$ {Convert.ToDecimal(salarioFuncionario).ToString("0.00")}");
