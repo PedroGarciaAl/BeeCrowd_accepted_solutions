d36156b4f570fa411217d81bd38dc958af403a99
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:18:55 -0300

    1009 Resolvido
    Solução problema 1009

---
+++ Csharp_accepted_solutions/Bee1009.cs
@@ -0,0 +1,13 @@
+﻿double salarioFixo, vendasTotal, salarioTotal, comissao;
+string nomeVendedor;
+
+comissao = 15/100;
+
+nomeVendedor = Console.ReadLine();
+salarioFixo = double.Parse(Console.ReadLine());
+vendasTotal = double.Parse(Console.ReadLine());
+
+vendasTotal *=  comissao;
+salarioTotal = salarioFixo + vendasTotal;
+
+Console.WriteLine($"TOTAL = R$ {Convert.ToDecimal(salarioTotal).ToString("0.00")}");
