6d5f11f1583b954e6d37c0f2dbbf4d39d2ca0541
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:12:13 -0300

    Create Bee1002.cs
    Solução problema 1002

---
+++ Csharp_accepted_solutions/Bee1002.cs
@@ -0,0 +1,8 @@
+﻿double area, raio, pi;
+pi = 3.14159;
+
+raio = double.Parse(Console.ReadLine());
+
+area = Math.Round(pi * (Math.Pow(raio,2)),4);
+
+Console.WriteLine($"A={Convert.ToDecimal(area).ToString("0.0000")}");
