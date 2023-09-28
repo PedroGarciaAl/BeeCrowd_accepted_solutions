c0d3ed73eadf8387d0991cd8457e36b6af21ad44
Pedro Garcia <132968371+PedroGarciaAl@users.noreply.github.com>
2023-09-27 23:17:54 -0300

    1007 Resolvido
    Solução problema 1007

---
+++ Csharp_accepted_solutions/Bee1007.cs
@@ -0,0 +1,10 @@
+﻿double a, b,c, d, diferenca;
+
+a = double.Parse(Console.ReadLine());
+b = double.Parse(Console.ReadLine());
+c = double.Parse(Console.ReadLine());
+d = double.Parse(Console.ReadLine());
+
+diferenca = (a * b) - ( c * d);
+
+Console.WriteLine($"DIFERENCA = {diferenca}");
